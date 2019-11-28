9ce2414a2b7e020cfe46e2f8d88ce10c6f3fc333
Gitana Brazlauskas <Bgitana@hotmail.com>
2019-11-27 20:15:11 -0500

    finished questions and timer code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,15 @@ $("#start").on("click", function() {
   game.loadQuestion();
 });
 
+// because this button tag does not exist on HTML page we need to grab a full document and not just class .answer-button' ALSO "(e)"= is used to store the EVENT...
+$(document).on("click", ".answer-button", function(e) {
+  game.clicked(e);
+});
+
+$(document).on("click", "#reset", function() {
+    game.reset();
+  });
+
 var questions = [
   {
     question: "How many legs does a Spider have?",
@@ -64,30 +73,36 @@ var questions = [
 
 var game = {
   questions: questions,
+  //   keeps track of what questions currently posted on the page
   currentQuestion: 0,
   counter: 30,
+  //   keeps track of number of correct answers
   correct: 0,
+  //   keeps track of number of incorrect answers
   incorrect: 0,
+  unanswered: 0,
+
   countdown: function() {
     game.counter--;
     // posting counted /lowered counter to the page - it will be posting each number as it goes down
-    $("#counter").html(game.counter);
+    $('#counter').html(game.counter);
     // below is telling if it's run out or not
     if (game.counter <= 0) {
       console.log(" TIME UP! ");
       game.timeUp();
     }
   },
+
   loadQuestion: function() {
     // this function will run every 1 second and will be lowering the counter
     timer = setInterval(game.countdown, 1000);
     // post a question to the page
-    $("#subwrapper").html(
-      "<h2>" + question[game.currentQuestion].question + "</h2>"
+    $('#subwrapper').html(
+      "<h2>" + questions[game.currentQuestion].question + "</h2>"
     );
     // post all answers to the page as button
     for (var i = 0; i < questions[game.currentQuestion].answers.length; i++) {
-      $("#subwrapper").append(
+      $('#subwrapper').append(
         '<button class="answer-button" id="button-' +
           i +
           ' "data-name="' +
@@ -98,11 +113,92 @@ var game = {
       );
     }
   },
-  nextQuestion: function() {},
-  timeUp: function() {},
-  results: function() {},
-  clicked: function() {},
-  answeredCorrectly: function() {},
-  answeredIncorrectly: function() {},
-  reset: function() {}
+
+  nextQuestion: function() {
+    //   this will set up the timer to  a new count
+    game.counter = 30;
+    //   this will print the new counter time on HTML
+    $('#counter').html(game.counter);
+     //   this will bring a new question from array - otherwise it will be looping through the same question over and over
+    game.currentQuestion++;
+    //   will load next question
+    game.loadQuestion();
+  },
+
+  timeUp: function() {
+    clearInterval(timer);
+    game.unanswered++;
+    $('#subwrapper').html('<h2>OUT OF TIME!!!</h2>');
+    $('#subwrapper').append('<h4>The correct answer was: <br> ' +questions[game.currentQuestion].correctAnswer+ '</h4>');
+        // will determine if this should take us the the next question or if we are on the last question - takes us to results screen
+if(game.currentQuestion == questions.length-1){
+    setTimeout(game.results, 3*1000);
+} else {
+setTimeout(game.nextQuestion, 3*1000);
+}
+  },
+
+  results: function() {
+      clearInterval(timer);
+      $('#subwrapper').html('<h2>ALL DONE!!!</h2>');
+      $('#subwrapper').append('<h4>Correct: '+game.correct+ '</h4>');
+      $('#subwrapper').append('<h4>Incorrect: '+game.incorrect+ '</h4>');
+      $('#subwrapper').append('<h4>Unanswered: '+game.unanswered+ '</h4>');
+      $('#subwrapper').append('<button id="reset">RESET</button>');
+  },
+
+  clicked: function(e) {
+    //   below is clearing interval so timer STOPS RUNNING
+    clearInterval(timer);
+    // this is catching item/target that has been clicked
+    if (
+      $(e.target).data("name") == questions[game.currentQuestion].correctAnswer
+    ) {
+      game.answeredCorrectly();
+    } else {
+      game.answeredIncorrectly();
+    }
+  },
+
+  answeredCorrectly: function() {
+    console.log("YOU GOT THIS!!!");
+// stop timer from ticking
+    clearInterval(timer);
+    //add to correct score number
+    game.correct++;
+    //prints a massage
+    $('#subwrapper').html('<h2>YOU GOT THIS!!!</h2>');
+    // will determine if this should take us the the next question or if we are on the last question - takes us to results screen
+if(game.currentQuestion == questions.length-1){
+    setTimeout(game.results, 3 *1000);
+} else {
+setTimeout(game.nextQuestion, 3 *1000);
+}
+  },
+
+  answeredIncorrectly: function() {
+    console.log("SORRY YOU ARE WRONG!!!");
+    // stop timer from ticking
+    clearInterval(timer);
+    //add to correct score number
+    game.incorrect++;
+    //prints a massage
+    $('#subwrapper').html('<h2>SORRY YOU GOT IT WRONG!!!</h2>');
+    $('#subwrapper').append('<h4>The correct answer was: <br>' +questions[game.currentQuestion].correctAnswer+ '</h4>');
+    // will determine if this should take us the the next question or if we are on the last question - takes us to results screen
+if(game.currentQuestion == questions.length-1){
+    setTimeout(game.results, 3 *1000);
+} else {
+setTimeout(game.nextQuestion, 3 *1000);
+}
+  },
+
+  reset: function() {
+    game.currentQuestion = 0,
+    game.counter = 30,
+    game.correct = 0,
+    game.incorrect = 0,
+    game.unanswered = 0,
+    game.loadQuestion();
+  }
 };
